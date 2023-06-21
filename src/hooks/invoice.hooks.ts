60a2bd2c638813de638ebab8cf722c5aa445733a
Bartosz Gerono <bgerono.gerono@xebia.com>
2023-06-21 22:11:01 +0200

    spront 2 cr amendments

--- src/hooks/invoice.hooks.ts
+++ src/hooks/invoice.hooks.ts
@@ -1,9 +1,10 @@
 import { Invoice } from '../models/invoice.model'
-import { useCallback } from 'react'
 
-export const createInvoice = useCallback(
-  (id: string, amount: number | null, created: string, validUntil: string): Invoice => {
-    return { id, amount, created, validUntil }
-  },
-  [],
-)
+export const createInvoice = (
+  id: string,
+  amount: number | null,
+  created: string,
+  validUntil: string,
+): Invoice => {
+  return { id, amount, created, validUntil }
+}
